35510ec3c1e8b0f2c34454c54a6543464e0e7f53
Eric Mann <eric@eamann.com>
2015-07-31 11:55:10 -0700

    Promisify

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ var lexicon = new rita.RiLexicon();
 var fs = require('fs');
 var r = rita.RiTa;
 var _ = require('lodash');
+var Promise = require( 'promise' );
 
 // helper added to all arrays for picking a random element
 Array.prototype.pick = function() {
   return this[Math.floor(Math.random()*this.length)];
 };
 
-function rhymeSayer (word) {
+function getRhyme( word ) {
   var a = lexicon.rhymes(word);
+
+  if ( a.length === 0 ) {
+    return false;
+  }
+
   for (var i = 0; i < a.length; i++) {
     var stress = r.getStresses(a[i]);
     if (r.getStresses(word) === stress) {
@@ -19,14 +25,47 @@ function rhymeSayer (word) {
   }
 }
 
+function rhymeSayer (word) {
+  return new Promise( function( fulfill, reject ) {
+    while( true ) {
+      var rhyme = getRhyme( word );
+      process.stdout.write( '.' );
+      if ( false === rhyme ) {
+        var a = [];
+        while ( a.length === 0 ) {
+          word = lexicon.randomWord();
+          a = lexicon.rhymes( word );
+          process.stdout.write( '|' );
+        }
+      } else {
+        fulfill( [word,rhyme] );
+        break;
+      }
+    }
+  } );
+}
+
 
 function catchUp (word) {
-  var rhyme1 = rhymeSayer(word);
-  var rhyme2 = rhymeSayer(rhyme1);
-  var line1 = 'Fuck being on some chill ' + rhyme1;
-  var line2 = 'We go 0 to 100 niggas, real ' + rhyme2;
-  return line1 + "\n" + line2;
+  return new Promise( function( fulfill, reject ) {
+    rhymeSayer( word ).done( function( pair ) {
+      word = pair[0];
+      var rhyme1 = pair[1];
+      rhymeSayer( rhyme1 ).done( function( pair2 ) {
+        rhyme1 = pair2[0];
+        var rhyme2 = pair2[1];
+
+        var line1 = 'Fuck being on some chill ' + rhyme1;
+        var line2 = 'We go 0 to 100 niggas, real ' + rhyme2;
+        fulfill( line1 + "\n" + line2 );
+      } );
+    } );
+  } );
+
 }
+
 for (var x = 0; x < 10; x++) {
-  console.log(catchUp(lexicon.randomWord()));
+  catchUp( lexicon.randomWord() ).done( function( statement ) {
+    console.log( statement );
+  } );
 }
